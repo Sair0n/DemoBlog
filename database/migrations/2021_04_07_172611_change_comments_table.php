1f2e117d5c520573101713c176cca9d1b1ba9817
Sairon <sairon@example.com>
2021-04-07 20:32:31 +0300

    Создание миграций

---
+++ database/migrations/2021_04_07_172611_change_comments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class ChangeCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            // свойство nullable добавлено post_id для запуска тестов в памяти
+            $table->foreignId('post_id')->nullable()->constrained()->cascadeOnDelete();
+
+            $table->foreignId('user_id')->nullable()->constrained()->nullOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            //
+        });
+    }
+}
